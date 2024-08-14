c1b9bfad5fd4173634e0f465a5c64f0c149bf5d9
luiza120 <134612996+luiza120@users.noreply.github.com>
2024-08-14 10:33:54 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,54 @@
+//Variaveis
+// Tipo Number
+
+const meuNumero = 6;
+const numeroUm = 1;
+const numeroDois = 2;
+
+console.log(numeroDois)
+
+const soma = 3 + 6
+console.log(soma)
+
+const subtracao = numeroUm - meuNumero;
+console.log(subtracao)
+
+const multiplicaçao = numeroDois * 10;
+console.log(multiplicacao);
+
+const divisao = meuNumero / numeroDois;
+console.log(divisao)
+
+//Float ( Número decimal )
+
+const numeroTres = 3.3;
+const numeroQuatro = .5 //mesma coisa que 0.5
+
+const operacaoM = numeroTres * numeroQuatro
+console.log(operacaoM)
+
+//NaN not a Number
+
+const operacaoN = "a" * meuNumero
+console.log(operacaoN)
+
+const novaOperacao = "a" + meuNumero
+console.log(novaOperacao)
+
+//string (texto)
+
+const meuNome = "Teotonio";
+const frase = 'Meu nome é: ';
+const issoNaoEUmNumero = '50';
+
+const minha Frase = frase + meuNome;
+console.log(minhaFrase);
+
+const compracao = 6 === meuNumero
+console.log(comparacao)
+
+const comparacaoII = "6" ===meuNumero
+console.log(comparacaoII)
+
+const compracaoIII = "6" == meuNumero;
+console.log(comparacaoIII);
